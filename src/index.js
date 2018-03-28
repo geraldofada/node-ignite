6202accec5d02f3a0e27bd4367e05c7fed58de28
Geraldo Fada <geraldofada.neto@gmail.com>
2018-03-27 22:09:57 -0300

    One example for testing

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// Example for testing
+
+const sum = require('./sum');
+
+console.log(sum(28, 14));
---
+++ src/sum.js
@@ -0,0 +1,7 @@
+// Example for testing
+
+function sum(a, b) {
+	return a + b;
+}
+
+module.exports = sum;
