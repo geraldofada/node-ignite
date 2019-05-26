84ab9e339f0a583fc39d85675bab57e6d97d52ab
Geraldo Fada <geraldofada.neto@gmail.com>
2019-05-26 19:16:06 -0300

    typescript files for testing

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import add from './math'
+
+console.log('Teste')
+
+const a = 10
+
+console.log(a)
+
+let b: number
+b = 10
+
+console.log(b)
+
+function generictest<T>(a: T): T {
+  return a
+}
+
+console.log(generictest<string>('test'))
+
+console.log(add(1, 2))
